28988a2ab948a285374c325e01e169a07183a2d4
Ordgod <34941987+Ordgod@users.noreply.github.com>
2018-10-08 18:05:23 +0800

    Update tf_poison_main.py
    add show_result fun

--- tf_poison_main.py
+++ tf_poison_main.py
@@ -1,4 +1,41 @@
 
+
+def show_result(x, changed_data, ckpt_path_final, ckpt_path_final_new, nb_success, nb_fail, target_class):
+    '''show result.
+    Args:
+        x: attack sample.
+        changed_data: those data in x_train which need to changed.
+        ckpt_path_final: where old model saved.
+        ckpt_path_final_new:where new model saved.
+    Returns:
+        nb_success: successful times.
+    '''
+
+    x_label_before = np.argmax(deep_cnn.softmax_preds(x, ckpt_path_final))
+    changed_labels_before = np.argmax(deep_cnn.softmax_preds(changed_data, ckpt_path_final), axis=1)
+
+    x_labels_after = np.argmax(deep_cnn.softmax_preds(x, ckpt_path_final_new))
+    changed_labels_after = np.argmax(deep_cnn.softmax_preds(changed_data, ckpt_path_final_new), axis=1)
+
+    print('\nold_label_of_x0: ', x_label_before,
+          '\nnew_label_of_x0: ', x_labels_after,
+          '\nold_label_of_changed_data: ', changed_labels_before[:5], # see whether changed data is misclassified by old model
+          '\nnew_label_of_changed_data: ', changed_labels_after[:5])
+    
+    if x_labels_after == target_class:
+        print('successful!!!')
+        nb_success += 1
+        
+    else:
+        print('failed......')
+        nb_fail +=1
+    print('number of x0 successful:', nb_success)
+    print('number of x0 failed:', nb_fail)
+    
+    return nb_success, nb_fail
+
+
+
 def start_train_data(train_data, train_labels, test_data, test_labels, ckpt_path, ckpt_path_final):  
     '''start train data and return accuracy.
     Args:
